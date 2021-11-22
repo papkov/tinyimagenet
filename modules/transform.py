bcba63c55b4e3db1a4b3396bc8395ac8c525a169
papkov <mikhail.papkov@gmail.com>
2021-11-22 18:54:11 +0200

    Add default torchvision transforms

--- modules/transform.py
+++ modules/transform.py
@@ -1,3 +1,4 @@
+import torchvision.transforms as T
 from albumentations import Compose, Normalize
 from albumentations.pytorch.transforms import ToTensorV2
 
@@ -19,3 +20,45 @@ def to_tensor_normalize() -> Transform:
         ]
     )
     return base_transform
+
+
+def to_tensor_normalize_torchvision() -> Transform:
+    """
+    :return: Torchvision transform [imagenet normalization, to tensor]
+    """
+    return T.Compose(
+        [
+            T.ToTensor(),
+            T.Normalize(
+                [0.485, 0.456, 0.406],
+                [0.229, 0.224, 0.225],
+            ),
+        ]
+    )
+
+
+def transforms_train_torchvision() -> Transform:
+    """
+    :return: Torchvision default training
+    """
+    return T.Compose(
+        [
+            T.Resize(256),
+            T.RandomCrop(176),
+            T.TrivialAugmentWide(),
+            to_tensor_normalize_torchvision(),
+        ]
+    )
+
+
+def transforms_valid_torchvision() -> Transform:
+    """
+    :return: Torchvision default validation
+    """
+    return T.Compose(
+        [
+            T.Resize(232),
+            T.RandomCrop(224),
+            to_tensor_normalize_torchvision(),
+        ]
+    )
